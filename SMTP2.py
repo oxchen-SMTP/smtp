b8257bf6f7a91cf5b3e7fcfc6e91bc73cf66dfab
Oliver Chen <oxchen@ad.unc.edu>
2023-02-16 20:58:19 -0500

    Drafted client with proper socket behavior and updated SMTP syntax

--- SMTP2.py
+++ SMTP2.py
@@ -6,12 +6,22 @@ Date: February 21, 2023
 
 import sys
 import os
+from enum import Enum
+import socket
+from socket import *
 
 """
 Checklist:
 
 - [x] Take hostname and port number from command line arguments
-- [ ]
+- [x] Prompt and receive email information from the user through the message
+- [x] Create a TCP socket to the SMTP server at the host and port number
+- [x] Handshake
+    - Receive and recognize a 220 code and confirm it's valid
+    - Reply with an SMTP HELO message (HELO <whitespace> <domain> <nullspace> <CRLF>)
+    - (Receive and reconize a 250 code)
+- [x] Format the message data from the user and send it to the server
+- [x] Receive and recognize a 221 code after quitting
 """
 
 do_debug = False
@@ -27,7 +37,6 @@ class ResponseParser:
     def __init__(self, message: str):
         self.stream = iter(message)
         self.next = next(self.stream)
-        self.code = -1
 
     def put_next(self):
         try:
@@ -78,97 +87,249 @@ class ResponseParser:
         return self.next == "\n"
 
 
+class PathParser:
+    SPECIAL = ("<", ">", "(", ")", "[", "]", "\\", ".", ",", ";", ":", "@", "\"")
+    SP = (" ", "\t")
+    ALPHA = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    DIGIT = "0123456789"
+
+    def __init__(self, path: str):
+        self.stream = iter(path)
+        self.nextc = next(self.stream)
+
+    def parse_path(self) -> str:
+        if self.local_part() != "":
+            return "Invalid local name"
+
+        if not self.consume("@"):
+            return "Missing @ in path"
+
+        if self.domain() != "":
+            return "Invalid domain name"
+
+        return ""
+
+    def putc(self):
+        try:
+            self.nextc = next(self.stream)
+        except StopIteration:
+            self.nextc = ""
+
+    def consume(self, s: str):
+        for c in s:
+            if self.nextc != c:
+                return False
+            self.putc()
+        return True
+
+    def local_part(self):
+        # <local-part> ::= <string>
+        return self.string()
+
+    def string(self):
+        # <string> ::= <char> | <char> <string>
+        res = self.char()
+        if res != "":
+            return "string"
+
+        self.string()
+
+        return ""
+
+    def char(self):
+        # <char> ::= any one of the printable ASCII characters, but not any of <special> or <SP>
+        if self.nextc in self.SPECIAL or self.nextc in self.SP:
+            return "char"
+        self.putc()
+        return ""
+
+    def domain(self):
+        # <domain> ::= <element> | <element> "." <domain>
+        res = self.element()
+        if res != "":
+            return res
+
+        if self.nextc == ".":
+            self.consume(".")
+            return self.domain()  # this element is fine, next one also needs to be
+
+        return ""
+
+    def element(self):
+        # <element> ::= <letter> | <name>
+        if self.letter() != "":
+            return "element"
+
+        self.name()
+
+        return ""
+
+    def name(self):
+        # <name> ::= <letter> <let-dig-str>
+        if self.let_dig_str():
+            return "name"
+
+        return ""
+
+    def letter(self):
+        # <letter> ::= any one of the 52 alphabetic characters A through Z in upper case and a through z in lower case
+        if self.nextc in self.ALPHA:
+            self.putc()
+            return ""
+        return "letter"
+
+    def let_dig_str(self):
+        # <let-dig-str> ::= <let-dig> | <let-dig> <let-dig-str>
+        if self.let_dig() != "":
+            return "let-dig-str"
+
+        self.let_dig_str()
+
+        return ""
+
+    def let_dig(self):
+        # <let-dig> ::= <letter> | <digit>
+        if self.letter() != "" and self.digit() != "":
+            return "let-dig"
+        return ""
+
+    def digit(self):
+        # <digit> ::= any one of the ten digits 0 through 9
+        if self.nextc in self.DIGIT:
+            self.putc()
+            return ""
+        return "digit"
+
+
 class Client:
-    def __init__(self, path):
-        self.state = 0
-        self.path = path
+    class State(Enum):
+        HELO = -1
+        FROM = 0
+        TO = 1
+        TOTAIL = 2
+        DATA = 3
+        QUIT = 99
+
+    def __init__(self, hostname, port):
+        self.state = self.State.HELO
+        self.server = (hostname, port)
+        with socket(AF_INET, SOCK_DGRAM) as client_socket:
+            client_socket.bind(self.server)
+            self.cli_socket = client_socket
+
+    def send(self, cmd: str):
+        self.cli_socket.sendto(cmd.encode(), self.server)
 
     def main(self):
-        lines = self.get_lines()
-        if lines is None:
+        msg_res = self.get_message()
+        if msg_res is None:
             return
+        from_, to, subj, msg = msg_res
 
-        for line in lines:
-            # debug(f"{self.state=}, {line=}")
-            match self.state:
-                case 0:
-                    # expecting From:
-                    self.parse_from(line)
-                case 1:
-                    # expecting To:
-                    if line.startswith("To: "):
-                        sys.stdout.write(f"RCPT TO: {line[4:].strip()}\n")
-                        self.react_to_response(250, 2)
-                    else:
-                        self.quit()
-                case 2:
-                    # expecting To: or data message
-                    if line.startswith("To: "):
-                        sys.stdout.write(f"RCPT TO: {line[4:].strip()}\n")
-                        self.react_to_response(250, 2)
-                    else:
-                        sys.stdout.write("DATA\n")
-                        self.react_to_response(354, 3)
-                        if self.state == -1:
-                            break
-                        self.read_message(line)
-                case 3:
-                    # expecting data message
-                    self.read_message(line)
-                case _:
-                    break
-        if self.state == 3:
-            # valid end state of message
-            self.close_message()
-        if self.state != -1:
-            self.quit()
+        self.react_to_response(220, self.State.FROM)
+        if self.state != self.State.QUIT:
+            self.send(f"HELO cs.unc.edu\n")
 
+        to_stream = iter(to)
+        while True:
+            # TODO: review SMTP and socket error handling + connection closing
+            try:
+                match self.state:
+                    case self.State.FROM:
+                        # command MAIL FROM:
+                        self.send(f"MAIL FROM: <{from_}>\n")
+                        self.react_to_response(250, self.State.TO)
+                    case self.State.TO:
+                        # command(s) RCPT TO:
+                        try:
+                            self.send(f"RCPT TO: <{next(to_stream)}>\n")
+                            self.react_to_response(250, self.State.TO)
+                        except StopIteration:
+                            self.state = self.State.DATA
+                    case self.State.DATA:
+                        # command DATA + message
+                        self.send("DATA\n")
+                        self.react_to_response(354, self.State.DATA)
+                        self.send(f"From: <{from_}>\n"
+                                  f"To: {', '.join([f'<{path}>' for path in to])}\n"
+                                  f"Subject: {subj}\n\n"
+                                  f"{msg}\n"
+                                  f".\n"
+                                  )
+                        self.react_to_response(250)
+                    case self.State.QUIT:
+                        self.send("QUIT\n")
+                        self.react_to_response(221)
+                        break
+            except socket.error as e:
+                print(f"Encountered a socket error: '{e}'")
+                break
 
-    def get_lines(self):
-        if self.path == "" or not os.path.exists(self.path):
-            return None
-        with open(self.path, "r+") as fp:
-            return fp.readlines()
-
-    def quit(self):
-        sys.stdout.write("QUIT\n")
-        self.state = -1
-
-    def parse_from(self, line):
-        if line.startswith("From: "):
-            sys.stdout.write(f"MAIL FROM: {line[6:].strip()}\n")
-            self.react_to_response(250, 1)
-        else:
-            self.quit()
-
-    def close_message(self):
-        sys.stdout.write(".\n")
-        self.react_to_response(250, 0)
-
-    def read_message(self, line: str):
-        if line.startswith("From: "):
-            # new message, reset state
-            self.close_message()
-            self.parse_from(line)
-        else:
-            self.state = 3
-            sys.stdout.write(line)
+        self.cli_socket.close()
 
     @staticmethod
-    def wait_for_response() -> (int, str):
-        for line in sys.stdin:
-            return ResponseParser(line).parse_code(), line
-        return -1, None
+    def get_message() -> (str, list[str], str, str):
+        try:
+            # Prompt From:
+            from_ = None
+            while from_ is None:
+                path = input("From:\n")
+                res = PathParser(path).parse_path()
+                if res != "":
+                    print(res)
+                else:
+                    from_ = path
 
-    def react_to_response(self, expected_code: int, next_state: int = -1):
-        (code, message) = Client.wait_for_response()
-        if message is not None:
-            sys.stderr.write(message)
-        debug(f"{code=}, {message=}")
+            # Prompt To:
+            to = None
+            while to is None:
+                paths = input("To:\n").split(", \t")
+                for p in paths:
+                    res = PathParser(p).parse_path()
+                    if res != "":
+                        print(res)
+                        continue
+                to = paths
+
+            # Prompt Subject:
+            subj = input("Subject:\n")
+
+            # Prompt Message:
+            print("Message:")
+            msg = ""
+            line = ""
+            while line != ".\n":
+                msg += line
+                line = input("")
+
+            return from_, to, subj, msg
+        except EOFError:
+            return None
+
+    def quit(self):
+        # self.send("QUIT\n")
+        self.state = self.State.QUIT
+
+    @staticmethod
+    def error(msg: str):
+        print(f"Encountered an SMTP error: '{msg}'")
+
+    def react_to_response(self, expected_code: int, next_state: State = State.QUIT) -> bool:
+        try:
+            response, serv_addr = self.cli_socket.recvfrom(2048)
+        except socket.error:
+            return False
+
+        message = response.decode()
+        code = ResponseParser(message).parse_code()
+
         if code != expected_code:
-            self.quit()
+            # self.quit()
+            self.error(message)
         else:
             self.state = next_state
+
+        return True
 
 
 def main():
@@ -182,8 +343,8 @@ def main():
             sys.stderr.write("Not enough arguments, expected hostname followed by port number\n")
         except ValueError:
             sys.stderr.write(f"Argument {sys.argv[2]} is not a valid port number\n")
-    # TODO: modify client to prompt message components
-    client = Client()
+
+    client = Client(hostname, port)
     client.main()
 
 
