8536b8190c27a6777dbabb4da82ee91e67363ac4
Oliver Chen <oxchen@ad.unc.edu>
2023-03-03 14:38:16 -0500

    Denesting and refactoring

--- Client.py
+++ Client.py
@@ -14,61 +14,133 @@ import logging
 logging.basicConfig(format="%(message)s", level=logging.INFO)
 
 
-class Client:
-    class State(Enum):
-        HELO = auto()
-        FROM = auto()
-        TO = auto()
-        DATA = auto()
-        QUIT = auto()
-        ERROR = auto()
+class State(Enum):
+    HELO = auto()
+    FROM = auto()
+    TO = auto()
+    DATA = auto()
+    QUIT = auto()
+    ERROR = auto()
 
+
+def parse_path(path) -> str:
+    buf = path
+    local_part = re.match(r"^[^<>()[\]@.,;:\\\"\s]+", buf)
+    if not local_part:
+        return "Invalid local name"
+    buf = buf.lstrip(local_part.group(0))
+
+    at_sign = re.match(r"^@", buf)
+    if not at_sign:
+        return "Missing @"
+    buf = buf.lstrip("@")
+
+    domain = re.match(r"^((?:(?:[a-zA-Z][a-zA-Z0-9]*)+\.)*(?:[a-zA-Z][a-zA-Z0-9]*)+)[ \t]*\n?$",
+                      buf)
+    if not domain:
+        return "Invalid domain name"
+
+    return ""
+
+
+def get_message() -> (str, list[str], str, str):
+    try:
+        from_ = None
+        while from_ is None:
+            print("From:")
+            line = sys.stdin.readline()
+            res = parse_path(line)
+            if res != "":
+                print(res)
+            else:
+                from_ = line.rstrip("\n")
+
+        to = None
+        while to is None:
+            print("To:")
+            paths = []
+            line = sys.stdin.readline()
+            for p in re.split(r",[ \t]*", line):
+                res = parse_path(p)
+                if res != "":
+                    print(res)
+                    to = None
+                    break
+                else:
+                    paths.append(p.rstrip("\n"))
+                    to = paths
+
+        print("Subject:")
+        subj = sys.stdin.readline().rstrip("\n")
+
+        print("Message:")
+        line = None
+        lines = []
+        while line != ".\n":
+            if line is not None:
+                lines.append(line.rstrip("\n"))
+            line = sys.stdin.readline()
+        msg = "\n".join(lines)
+
+        return from_, to, subj, msg
+    except EOFError:
+        return None
+
+
+def parse_code(message: str) -> int:
+    match = re.match(r"^(\d{3})\s+.*\n$", message)
+    if not match:
+        return -1
+    return int(match.group(1))
+
+
+class Client:
     def __init__(self, hostname, port):
-        self.state = self.State.HELO
+        self.state = State.HELO
         self.server = (hostname, port)
         try:
             self.cli_socket = socket(AF_INET, SOCK_STREAM)
             self.cli_socket.connect(self.server)
             logging.debug(f"connected to {hostname}")
         except OSError:
-            self.state = self.State.ERROR
+            self.state = State.ERROR
 
     def send(self, s: str):
         logging.debug(f"sending: {s}".rstrip())
         self.cli_socket.send(s.encode())
 
     def main(self):
-        if self.state == self.state.ERROR:
+        if self.state == State.ERROR:
             return
 
-        msg_res = self.get_message()
+        msg_res = get_message()
         if msg_res is None:
             return
         from_, to, subj, msg = msg_res
 
-        self.react_to_response(220, self.State.FROM)
-        if self.state not in [self.State.QUIT, self.State.ERROR]:
+        self.react_to_response(220, State.FROM)
+        if self.state not in [State.QUIT, State.ERROR]:
             self.send(f"HELO {gethostname()}\n")
 
         to_stream = iter(to)
-        while True:
+        while self.state not in (State.QUIT, State.ERROR):
             try:
                 match self.state:
-                    case self.State.FROM:
+                    case State.FROM:
                         # command MAIL FROM:
                         self.send(f"MAIL FROM: <{from_}>\n")
-                        self.react_to_response(250, self.State.TO)
-                    case self.State.TO:
+                        self.react_to_response(250, State.TO)
+                    case State.TO:
                         # command(s) RCPT TO:
                         try:
                             self.send(f"RCPT TO: <{next(to_stream)}>\n")
-                            self.react_to_response(250, self.State.TO)
+                            self.react_to_response(250, State.TO)
                         except StopIteration:
-                            self.react_to_response(250, self.State.DATA)
-                    case self.State.DATA:
+                            self.react_to_response(250, State.DATA)
+                    case State.DATA:
                         # command DATA + message
                         self.send("DATA\n")
-                        self.react_to_response(354, self.State.DATA)
+                        self.react_to_response(354, State.DATA)
                         self.send(f"From: <{from_}>\n"
                                   f"To: {', '.join([f'<{path}>' for path in to])}\n"
                                   f"Subject: {subj}\n\n"
@@ -76,37 +148,14 @@ class Client:
                                   f".\n"
                                   )
                         self.react_to_response(250)
-                    case self.State.QUIT:
+                    case State.QUIT:
                         self.send("QUIT\n")
                         self.react_to_response(221)
-                        break
-                    case self.State.ERROR:
-                        break
             except OSError as e:
                 print(f"Encountered a socket error: '{e}'")
                 break
 
         self.cli_socket.close()
-
-    @staticmethod
-    def parse_path(path) -> str:
-        buf = path
-        local_part = re.match(r"^[^<>()[\]@.,;:\\\"\s]+", buf)
-        if not local_part:
-            return "Invalid local name"
-        buf = buf.lstrip(local_part.group(0))
-
-        at_sign = re.match(r"^@", buf)
-        if not at_sign:
-            return "Missing @"
-        buf = buf.lstrip("@")
-
-        domain = re.match(r"^((?:(?:[a-zA-Z][a-zA-Z0-9]*)+\.)*(?:[a-zA-Z][a-zA-Z0-9]*)+)[ \t]*\n?$",
-                          buf)
-        if not domain:
-            return "Invalid domain name"
-
-        return ""
 
     def get_input(self, prompt: str):
         print(prompt)
@@ -115,67 +164,13 @@ class Client:
             out = line.rstrip("\n")
             break
         if out is None:
-            self.state = self.State.ERROR
+            self.state = State.ERROR
             return None
         return out
 
-    def get_message(self) -> (str, list[str], str, str):
-        try:
-            from_ = None
-            while from_ is None:
-                print("From:")
-                line = sys.stdin.readline()
-                res = Client.parse_path(line)
-                if res != "":
-                    print(res)
-                else:
-                    from_ = line.rstrip("\n")
-
-            to = None
-            while to is None:
-                print("To:")
-                paths = []
-                line = sys.stdin.readline()
-                for p in re.split(r",[ \t]*", line):
-                    res = Client.parse_path(p)
-                    if res != "":
-                        print(res)
-                        to = None
-                        break
-                    else:
-                        paths.append(p.rstrip("\n"))
-                        to = paths
-
-            print("Subject:")
-            subj = sys.stdin.readline().rstrip("\n")
-
-            print("Message:")
-            line = None
-            lines = []
-            while line != ".\n":
-                if line is not None:
-                    lines.append(line.rstrip("\n"))
-                line = sys.stdin.readline()
-            msg = "\n".join(lines)
-
-            return from_, to, subj, msg
-        except EOFError:
-            return None
-
-    def quit(self):
-        # self.send("QUIT\n")
-        self.state = self.State.QUIT
-
     def error(self, msg: str):
         print(f"Encountered an SMTP error: {msg}".rstrip())
-        self.state = self.State.ERROR
-
-    @staticmethod
-    def parse_code(message: str) -> int:
-        match = re.match(r"^(\d{3})\s+.*\n$", message)
-        if not match:
-            return -1
-        return int(match.group(1))
+        self.state = State.ERROR
 
     def react_to_response(self, expected_code: int, next_state: State = State.QUIT) -> bool:
         try:
@@ -185,7 +180,7 @@ class Client:
             return False
 
         message = response.decode()
-        code = self.parse_code(message)
+        code = parse_code(message)
         logging.debug(f"{self.state}")
         logging.debug(f"{expected_code=}, {code=}, {next_state=}")
 
--- Server.py
+++ Server.py
@@ -44,6 +44,48 @@ def send(sock: socket, s: str):
     sock.send(f"{s}\n".encode())
 
 
+def parse_cmd(line: str) -> Command:
+    # HELO
+    if re.match(r"^HELO\s+", line):
+        return Command.HELO
+
+    # MAIL FROM
+    if re.match(r"^MAIL\s+FROM:", line):
+        return Command.MAIL
+
+    # RCPT TO
+    if re.match(r"^RCPT\s+TO:", line):
+        return Command.RCPT
+
+    # DATA
+    if re.match(r"^DATA\s*", line):
+        return Command.DATA
+
+    # QUIT
+    if re.match(r"^QUIT\s*", line):
+        return Command.QUIT
+
+    return Command.UNRECOGNIZED
+
+
+def code(num: int, data: str = "") -> str:
+    match num:
+        case 220:
+            return f"220 {gethostname()}"
+        case 221:
+            return f"221 {gethostname()} closing connection"
+        case 250:
+            return f"250 {data}"
+        case 354:
+            return "354 Start mail input; end with <CRLF>.<CRLF>"
+        case 500:
+            return "500 Syntax error: command unrecognized"
+        case 501:
+            return "501 Syntax error in parameters or arguments"
+        case 503:
+            return "503 Bad sequence of commands"
+
+
 class Server:
     SPECIAL = ("<", ">", "(", ")", "[", "]", "\\", ".", ",", ";", ":", "@", "\"")
     SP = (" ", "\t")
@@ -70,71 +112,11 @@ class Server:
                         self.command_buffer.append(m)
                         logging.debug(f"received: {m}".rstrip("\n"))
                 if self.state == State.DATABODY:
-                    # captures body in 1
-                    body = ""
-                    try:
-                        line = ""
-                        while line != "From:":
-                            body += line
-                            line = self.command_buffer.pop(0)
-                        body += line
-                    except IndexError:
-                        pass
-                    re_match = re.match(r"^((.*\n)*)\.\n", body)
-                    if re_match:
-                        self.send(self.code(250, "OK"))
-                        body = re_match.group(1)
-                        for fpath in self.forward_path_strs:
-                            with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "forward", fpath),
-                                      "a+") as fp:
-                                logging.debug(f"writing {body} to ./forward/{fpath}")
-                                fp.write(body)
-                        self.forward_path_strs = set()
-                        self.state = State.MAIL
-                    else:
-                        self.send(self.code(501))
+                    self.interpret_body()
                 else:
-                    message = self.command_buffer.pop(0)
-                    command = self.parse_cmd(message)
-                    logging.debug(command)
-                    if self.state not in command.states:
-                        self.send(self.code(503))
-                        self.state = State.MAIL
-                    else:
-                        if command == Command.UNRECOGNIZED:
-                            self.send(self.code(500))
-                            self.state = State.MAIL
-                        else:
-                            re_match = re.match(command.pattern, message)
-                            if re_match:
-                                match command:
-                                    case Command.HELO:
-                                        # captures hostname/domain name in 1
-                                        client_hostname = re_match.group(1)
-                                        self.send(self.code(250, f"Hello {client_hostname} pleased to meet you"))
-                                        self.state = State.MAIL
-                                    case Command.MAIL:
-                                        # captures local-part in 1, domain name in 2
-                                        logging.debug(re_match.groups())
-                                        self.send(self.code(250, f"OK"))
-                                        self.forward_path_strs = set()
-                                        self.state = State.RCPT
-                                    case Command.RCPT:
-                                        # captures local-part in 1, domain name in 2
-                                        logging.debug(re_match.groups())
-                                        self.send(self.code(250, f"OK"))
-                                        domain = re_match.group(2)
-                                        self.forward_path_strs.add(domain)
-                                        self.state = State.RCPTDATA
-                                    case Command.DATA:
-                                        self.send(self.code(354))
-                                        self.state = State.DATABODY
-                                    case Command.QUIT:
-                                        # self.send(self.code(221))
-                                        self.state = State.QUIT
-                            else:
-                                self.send(self.code(501))
-            self.send(self.code(221))
+                    self.interpret_cmd()
+
+            self.send(code(221))
         except OSError as e:
             print(f"Encountered a socket error during execution of the program: {e}")
 
@@ -142,47 +124,70 @@ class Server:
         logging.debug(f"sending: {s}".rstrip())
         send(self.conn_socket, s)
 
-    @staticmethod
-    def parse_cmd(line: str) -> Command:
-        # HELO
-        if re.match(r"^HELO\s+", line):
-            return Command.HELO
+    def interpret_body(self):
+        body = ""
+        try:
+            line = ""
+            while line != "From:":
+                body += line
+                line = self.command_buffer.pop(0)
+            body += line
+        except IndexError:
+            pass
+        re_match = re.match(r"^((.*\n)*)\.\n", body)
+        if not re_match:
+            self.send(code(501))
+            return
+        self.send(code(250, "OK"))
+        body = re_match.group(1)
+        for fpath in self.forward_path_strs:
+            with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "forward", fpath),
+                      "a+") as fp:
+                logging.debug(f"writing {body} to ./forward/{fpath}")
+                fp.write(body)
+        self.forward_path_strs = set()
+        self.state = State.MAIL
 
-        # MAIL FROM
-        if re.match(r"^MAIL\s+FROM:", line):
-            return Command.MAIL
-
-        # RCPT TO
-        if re.match(r"^RCPT\s+TO:", line):
-            return Command.RCPT
-
-        # DATA
-        if re.match(r"^DATA\s*", line):
-            return Command.DATA
-
-        # QUIT
-        if re.match(r"^QUIT\s*", line):
-            return Command.QUIT
-
-        return Command.UNRECOGNIZED
-
-    @staticmethod
-    def code(num: int, data: str = "") -> str:
-        match num:
-            case 220:
-                return f"220 {gethostname()}"
-            case 221:
-                return f"221 {gethostname()} closing connection"
-            case 250:
-                return f"250 {data}"
-            case 354:
-                return "354 Start mail input; end with <CRLF>.<CRLF>"
-            case 500:
-                return "500 Syntax error: command unrecognized"
-            case 501:
-                return "501 Syntax error in parameters or arguments"
-            case 503:
-                return "503 Bad sequence of commands"
+    def interpret_cmd(self):
+        message = self.command_buffer.pop(0)
+        command = parse_cmd(message)
+        logging.debug(command)
+        if self.state not in command.states:
+            self.send(code(503))
+            self.state = State.MAIL
+            return
+        if command == Command.UNRECOGNIZED:
+            self.send(code(500))
+            self.state = State.MAIL
+            return
+        re_match = re.match(command.pattern, message)
+        if not re_match:
+            self.send(code(501))
+            return
+        match command:
+            case Command.HELO:
+                # captures hostname/domain name in 1
+                client_hostname = re_match.group(1)
+                self.send(code(250, f"Hello {client_hostname} pleased to meet you"))
+                self.state = State.MAIL
+            case Command.MAIL:
+                # captures local-part in 1, domain name in 2
+                logging.debug(re_match.groups())
+                self.send(code(250, f"mail from OK"))
+                self.forward_path_strs = set()
+                self.state = State.RCPT
+            case Command.RCPT:
+                # captures local-part in 1, domain name in 2
+                logging.debug(re_match.groups())
+                self.send(code(250, f"rcpt to OK"))
+                domain = re_match.group(2)
+                self.forward_path_strs.add(domain)
+                self.state = State.RCPTDATA
+            case Command.DATA:
+                self.send(code(354))
+                self.state = State.DATABODY
+            case Command.QUIT:
+                self.state = State.QUIT
 
 
 def main():
@@ -196,28 +201,28 @@ def main():
 
     if port < 1 or port > 65536:
         logging.debug(f"Invalid port: {port}")
-    else:
-        try:
-            with socket(AF_INET, SOCK_STREAM) as serv_socket:
-                serv_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-                serv_socket.bind(("", port))
-                serv_socket.listen(1)
-                logging.debug(f"created server socket on port {port}")
+        return
+    try:
+        with socket(AF_INET, SOCK_STREAM) as serv_socket:
+            serv_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+            serv_socket.bind(("", port))
+            serv_socket.listen(1)
+            logging.debug(f"created server socket on port {port}")
 
-                while True:
-                    try:
-                        conn_socket, addr = serv_socket.accept()
-                        logging.debug("accepted connection, handshaking")
-                        # conn_socket listens to cli_socket
-                        parser = Server(conn_socket)
-                        parser.main()
-                    except OSError as e:
-                        print(f"Encountered an error while establishing the connection socket: {e}")
-                    finally:
-                        conn_socket.close()
-                        logging.debug("closed connection with client")
-        except OSError as e:
-            print(f"Encountered an error while establishing the welcoming socket: {e}")
+            while True:
+                try:
+                    conn_socket, addr = serv_socket.accept()
+                    logging.debug("accepted connection, handshaking")
+                    # conn_socket listens to cli_socket
+                    parser = Server(conn_socket)
+                    parser.main()
+                except OSError as e:
+                    print(f"Encountered an error while establishing the connection socket: {e}")
+                finally:
+                    conn_socket.close()
+                    logging.debug("closed connection with client")
+    except OSError as e:
+        print(f"Encountered an error while establishing the welcoming socket: {e}")
 
 
 if __name__ == "__main__":
